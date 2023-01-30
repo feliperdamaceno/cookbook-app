47eac814a1822ef956d97aac873ba659ede123e7
feliperdamaceno <feliperdamaceno@sent.com>
2023-01-30 15:20:31 +0000

    feat: add gh-pages

--- src/helpers/calculateTime.js
+++
@@ -1,5 +0,0 @@
-export default function calculateTime(time) {
-  const hours = Math.floor(time / 60)
-  const minutes = time % 60
-  return [hours, minutes]
-}
--- src/helpers/generateTimeString.js
+++
@@ -1,6 +0,0 @@
-export default function generateTimeString(hours, minutes, endString = '') {
-  let timeString = ''
-  if (hours) timeString += hours + 'h '
-  if (minutes) timeString += minutes + 'min '
-  return timeString + endString
-}
